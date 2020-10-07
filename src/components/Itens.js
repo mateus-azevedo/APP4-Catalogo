182925483aa4be03b57beacf2fa34c2b1cef3093
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-07 12:51:36 -0300

    criado component Itens

---
+++ src/components/Itens.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class Itens extends Component {
+
+  render() {
+    return (
+      <Text>Detalhe do Produto</Text>
+    );
+  }
+}
--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import {
-  Text
+  View
 } from 'react-native';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
-
-  constructor(props) {
-    super(props);
-    console.log('Construindo a aplicação');
-  }
-
-  UNSAFE_componentWillMount() {
-    console.log('Fazer alguma coisa antes de renderizar');
-  }
-
-  componentDidMount() {
-    console.log('Fazer alguma coisa depois da renderização');
-  }
 
   render() {
     console.log('Objeto é renderizado');
     return (
-      <Text>Teste de Importação de componente</Text>
+      <View>
+        <Itens />
+        <Itens />
+        <Itens />
+      </View>
     );
   }
 }
