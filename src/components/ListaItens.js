f664c5e8cfc2e2defcb5cb29022f9a31ef0f6122
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-09 14:43:12 -0300

    implementando o axios no projeto

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import {
   View
 } from 'react-native';
+import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+
+  UNSAFE_componentWillMout() {
+    //requisição HTTP
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then(response => { console.log(response); })
+      .catch(() => { console.log('Erro ao recuperar os dados'); });
+  }
 
   render() {
     console.log('Objeto é renderizado');
