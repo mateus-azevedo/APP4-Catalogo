25417f5982563dac7cb0947dae559050468d5bd8
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-13 00:46:33 -0300

    msotrando todos os dados do array na tela

--- src/components/Itens.js
+++ src/components/Itens.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import {
-  Text
+  Text,
+  View,
+  Image
 } from 'react-native';
 
 export default class Itens extends Component {
 
   render() {
     return (
-      <Text>Detalhe do Produto</Text>
+      <View>
+        <Image style={{ height: 100, width: 100 }} source={{ uri: this.props.item.foto }} />
+        <Text>{this.props.item.titulo}</Text>
+        <Text>{this.props.item.valor}</Text>
+        <Text>{this.props.item.local_anuncio}</Text>
+        <Text>{this.props.item.data_publicacao}</Text>
+      </View>
     );
   }
 }
